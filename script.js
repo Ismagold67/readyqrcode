d046aed0e6a833cabaccb819d21ef2dc624aa9de
IsmaGold67 <ismaelroberto67@gmail.com>
2024-11-17 16:56:06 -0400

    atualiza .gitignore

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ async function handleQrCodeScan(qrCodeMessage) {
             document.getElementById('checkInStatus').textContent = 'Status: Aguardando registro de presença';
             document.getElementById('checkInButton').disabled = false;
             document.getElementById('personInfo').style.display = 'block';
-            stopScanning()
+            stopScanning();
         }
     } catch (error) {
         showError('Erro ao processar QR Code: ' + error.message);
